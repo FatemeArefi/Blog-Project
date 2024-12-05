1430b4b7828f3b41eaf0521ff7115a4d2d1e01e7
FatemeArefi <f.arefi982@gmail.com>
2024-12-05 21:52:53 +0330

    edited files

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@emotion/react";
-import theme from "./mui/theme";
+import theme from "./mui/theme.js";
 
 import "./styles/index.css";
 import "./styles/fonts.css";
