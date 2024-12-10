d095c3a65960c570db3ed8c36a6edd743aeb4d86
FatemeArefi <f.arefi982@gmail.com>
2024-12-10 23:13:16 +0330

    comment sending added

--- src/components/layout/graphql/mutation.js
+++ src/components/layout/graphql/mutation.js
@@ -1,22 +1,19 @@
 import { gql } from "@apollo/client";
 
 const SEND_COMMENT = gql`
-  mutation MyMutation(
+  mutation send_comment(
     $name: String!
     $email: String!
     $text: String!
     $slug: String!
   ) {
-    createComment(
-      data: {
-        name: $name
-        email: $email
-        text: $text
-        post: { connect: { slug: $slug } }
-      }
-    ) {
-      id
-    }
+     {
+  createComment(
+    data: {name: $name, email: $email, text: $text, post: {connect: {slug: $slug}}}
+  ) {
+    id
+  }
+}
   }
 `;
 
